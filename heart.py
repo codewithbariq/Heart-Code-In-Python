938af339065eef9f5ec77073b45a77a7d88e2a84
CodeWithBariq <bariqmalik04@outlook.com>
2025-08-15 14:59:39 +0530

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,19 @@
+import math
+from turtle import *
+def hearta(k):
+    return 15*math.sin(k)**3
+def heartb(k):
+    return 13*math.cos(k)-5*\
+        math.cos(2*k)-2*\
+            math.cos(3*k)-\
+                math.cos(4*k)
+
+
+speed(0)
+bgcolor('black')
+for i in range(6000):
+    goto(hearta(i)*15, heartb(i)*15)
+    for j in range(5):
+        color("#f73487")
+        goto(0, 0)
+done()
